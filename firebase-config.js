cb8b2125d3a515cb00cb00338d77cf2c22db1dbc
WallMuse <ignacio.vanderdys@sms87.com>
2025-05-22 21:41:46 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+firebase.initializeApp({
+  apiKey: "AIzaSyBud6nrSTh5SBTmwn8QNuNlWRiHtUjgMM8",
+  authDomain: "wallmuse-1ef98.firebaseapp.com",
+  projectId: "wallmuse-1ef98",
+  storageBucket: "wallmuse-1ef98.firebasestorage.app",
+  messagingSenderId: "140963790816",
+  appId: "1:140963790816:web:70cb65bb01be0749eab585"
+});
